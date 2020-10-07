806eab59dd38e0bafee8fb822561f44f664e5388
GatienBouyssou <gatien.bouyssou@etu.unilim.fr>
2020-10-07 11:34:57 +0100

    Adding a simple solution

---
+++ NearestNeighbour.py
@@ -0,0 +1,51 @@
+import math
+import csv
+import numpy as np
+
+data = np.loadtxt("data.txt", delimiter=", ")
+
+
+def euclideanDistance(currentPoint, line):
+    return math.sqrt(pow(currentPoint[0] - line[1], 2) + pow(currentPoint[1] - line[2], 2))
+
+
+# euclidean dist without sqrt + size * 0.1
+def distance(currentPoint, line):
+    return (pow(currentPoint[0] - line[1], 2) + pow(currentPoint[1] - line[2], 2)) + line[3]/10
+
+
+def getNextPoint(currentPoint):
+    global data
+    bestDistance = float('inf')
+    bestItem = -1
+    for i, line in enumerate(data):
+        currentDistance = distance(currentPoint, line)
+        if bestDistance > currentDistance:
+            bestDistance = currentDistance
+            bestItem = i
+    result = data[bestItem]
+    data = np.delete(data, bestItem, 0)
+    return result
+
+
+if __name__ == '__main__':
+    currentPoint = (0,0)
+    currentDistanceDone = 0
+    resultArray = []
+
+    while currentDistanceDone < 10000:
+        nextPoint = getNextPoint(currentPoint)
+        currentDistanceDone += euclideanDistance(currentPoint, nextPoint)
+        resultArray.append(nextPoint[0])
+        currentPoint = (nextPoint[1], nextPoint[2])
+        print("res for this round")
+        print(currentPoint)
+        print(currentDistanceDone)
+        print(nextPoint[0])
+    resultArray.pop()
+
+    with open('result.csv', 'w+') as csvfile:
+        writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for cellId in resultArray:
+            writer.writerow([str(int(cellId))])
